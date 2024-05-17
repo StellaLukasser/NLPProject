30ad36c3aa5a2adabc855412e8187dc0e6106c37
stell <stella.lukasser@student.tugraz.at>
2024-05-17 10:33:52 +0200

    task 2 sequences and tokenization

--- task2.py
+++ task2.py
@@ -1,5 +1,7 @@
 import os
 import re
+from keras.src.preprocessing.text import Tokenizer
+
 
 path = os.curdir + "/data"
 file1_kogler = path + "/data_stage_2/data_stage2_1_kogler.txt"
@@ -29,7 +31,18 @@ def pre_processing(text):
     text = re.sub("\s+", " ", text).strip()
     #convert words and punctuations to indices
     text = re.findall(r"[\w']+|[.,!?;]", text)
-    print(text)
+    #print(text)
+    return text
+
+
+def split_text_into_sequences(text, length):
+    sequences = list()
+    for i in range(length, len(text)):
+        sequence = text[i - length:i]
+        line = ' '.join(sequence)
+        sequences.append(line)
+    return sequences
+
 
 def main():
     #read text
@@ -38,7 +51,24 @@ def main():
     #process data
     text_kogler = pre_processing(text_kogler)
     text_kickl = pre_processing(text_kickl)
-    #TODO sequences
+    print(text_kickl)
+    print(text_kogler)
+
+    #sequences
+    sequence_length = 30
+    seq_kogler = split_text_into_sequences(text_kogler, sequence_length + 1)
+    seq_kickl = split_text_into_sequences(text_kickl, sequence_length + 1)
+    print(seq_kogler[:10])
+    print(seq_kickl[:10])
+
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(seq_kogler)
+
+    # tokenize sequences
+    sequences_tokenized_kogler = tokenizer.texts_to_sequences(seq_kogler)
+    print(sequences_tokenized_kogler[:10])
+    sequences_tokenized_kickl = tokenizer.texts_to_sequences(seq_kickl)
+    print(sequences_tokenized_kickl[:10])
 
 if __name__ == '__main__':
     main()
