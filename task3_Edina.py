0ec417ccb6801f4256e2bf9e39fdcf5bb0a8af3f
Edina Mulahasanovic <emulahasanovic@student.tugraz.at>
2024-06-06 17:34:13 +0200

    task3 Edina preprocessing

--- task3_Edina.py
+++ task3_Edina.py
@@ -36,6 +36,7 @@ def clean_tweet_trump(tweet):
     tweet = re.sub("\w+\.com", " ", tweet)
     tweet = re.sub("\s+", " ", tweet).strip()
     tweet = re.sub(r'RT : *', '', tweet)
+    tweet = re.sub(r":", "", tweet)
     # â€™ as '
     tweet = re.sub(r"â€™", "'", tweet)
     tweet = re.sub(r"â€“", "—", tweet)
@@ -51,18 +52,13 @@ def clean_tweet_trump(tweet):
     #tweet = re.sub(r'\W+', ' ', tweet)
     #remove new line
     tweet = tweet.rstrip()
-    # Tweets longer than 50 chars
-    #tweet = tweet[tweet.map(len) > 50]
+
     # print(original)
     # print(tweet)
     # print()
     return tweet
 
-
-def main():
-    # with open('data/data_stage_3/data_stage3_2_trump.xlsx', encoding='utf-8') as f:
-    #     df_trump = pd.read_excel(f)
-
+def preprocessing():
     df_musk = pd.read_excel('data/data_stage_3/data_stage3_1_musk.xlsx', header=None, engine='openpyxl')
     tweets_musk = df_musk.iloc[:, 0]
 
@@ -76,15 +72,47 @@ def main():
     tweets_cleaned_musk.dropna(inplace=True)
     tweets_cleaned_trump.dropna(inplace=True)
 
+    #clean unnecessary chars from html encoding
     chars = sorted(list(set(''.join(tweets_cleaned_trump))))
     char_indices = dict((cd, i) for i, cd in enumerate(chars))
     print("Chars in trup text", char_indices)
 
-    #removing unwated 163-85
+    #removing unwanted chars 163-85 trump
     for c in chars[-78:]:
         tweets_cleaned_trump = tweets_cleaned_trump.str.replace(c,'')
+    
+    # Remove tweets longer than 50 chars
+    tweets_cleaned_trump = tweets_cleaned_trump[tweets_cleaned_trump.map(len) > 20]
 
-    print(tweets_cleaned_trump.head(50))
+    chars = sorted(list(set(''.join(tweets_cleaned_musk))))
+    char_indices = dict((cd, i) for i, cd in enumerate(chars))
+    print("Chars in musk text", char_indices)
+    #removing unwanted chars 205-90 trump
+    for c in chars[-116:]:
+        tweets_cleaned_musk = tweets_cleaned_musk.str.replace(c,'')
+
+    # Remove tweets longer than 50 chars
+    tweets_cleaned_musk = tweets_cleaned_musk[tweets_cleaned_musk.map(len) > 20]
+
+    tweets_cleaned_musk.dropna(inplace=True)
+    tweets_cleaned_trump.dropna(inplace=True)
+
+    #print(tweets_cleaned_trump.head(50))
+
+    tweets_cleaned_trump.to_csv('tweets_cleaned_trump.csv', index=False, encoding='utf-8')
+    tweets_cleaned_musk.to_csv('tweets_cleaned_musk.csv', index=False, encoding='utf-8')
+    print('yeah')
+    return tweets_cleaned_musk, tweets_cleaned_trump
+    
+
+def main():
+    #with open('data/data_stage_3/data_stage3_2_trump.xlsx', encoding='utf-8') as f:
+    #    df_trump = pd.read_excel(f)
+    preprocessing()
+    #trump = pd.read_csv('tweets_cleaned_trump.csv', encoding='utf-8', sep=':')
+    #musk = pd.read_csv('tweets_cleaned_trump.csv', encoding='utf-8', sep=':')
+
+
 
 
 if __name__ == '__main__':
